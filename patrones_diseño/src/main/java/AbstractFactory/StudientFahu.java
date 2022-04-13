ff0f35d8cca41a27bb2a183da198bfddd7cd75f4
Maximiliano Valenzuela <maximiliano.valenzuela.s@usach.cl>
2022-04-13 10:56:17 -0400

    Implementar getters y setters para las clases StudientFing y StudientFahu

--- patrones_diseño/src/main/java/AbstractFactory/StudientFahu.java
+++ patrones_diseño/src/main/java/AbstractFactory/StudientFahu.java
@@ -13,4 +13,20 @@ public class StudientFahu implements Studient{
     public void asigmentSubject() {
         System.out.println("se ha asignado un horario al estudiante "+this.name+" del nivel "+this.name+" de la facultad de humanidades");
     }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- patrones_diseño/src/main/java/AbstractFactory/StudientFing.java
+++ patrones_diseño/src/main/java/AbstractFactory/StudientFing.java
@@ -13,4 +13,20 @@ public class StudientFing implements Studient{
     public void asigmentSubject() {
         System.out.println("se ha asignado un horario al estudiante "+this.name+" del nivel "+this.name+" de la facultad de ingenieria");
     }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
