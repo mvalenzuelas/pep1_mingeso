e02287dc44789dcbf480b8dd68da52657ce54eba
Maximiliano Valenzuela <maximiliano.valenzuela.s@usach.cl>
2022-04-13 10:53:17 -0400

    Implementar getter y setter para la clases ProfessorFing y ProfessorFahu

--- patrones_diseño/src/main/java/AbstractFactory/ProfessorFahu.java
+++ patrones_diseño/src/main/java/AbstractFactory/ProfessorFahu.java
@@ -21,4 +21,33 @@ public class ProfessorFahu implements Professor{
     public void assignmentAssignment() {
         System.out.println("Se ha asignado al profesor de la facultad de humanidades"+this.name+" de la carrera profesional "+this.career+"y la especialidad "+this.specialty+" las asignaturas que imparte");
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCareer() {
+        return career;
+    }
+
+    public void setCareer(String career) {
+        this.career = career;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        if (specialty!= null){
+            this.specialty = specialty;
+        }
+        else{
+            this.specialty="Sin especialidad";
+        }
+    }
 }
--- patrones_diseño/src/main/java/AbstractFactory/ProfessorFing.java
+++ patrones_diseño/src/main/java/AbstractFactory/ProfessorFing.java
@@ -22,4 +22,32 @@ public class ProfessorFing implements Professor{
         System.out.println("Se ha asignado al profesor de la facultad de ingenieria"+this.name+" de la carrera profesional "+this.career+"y la especialidad "+this.specialty+" las asignaturas que imparte");
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCareer() {
+        return career;
+    }
+
+    public void setCareer(String career) {
+        this.career = career;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        if (specialty!= null){
+            this.specialty = specialty;
+        }
+        else{
+            this.specialty="Sin especialidad";
+        }
+    }
 }
