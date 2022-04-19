746e481149ba1eb6f0c56b653d4444ee4382dc7e
Maximiliano Valenzuela <maximiliano.valenzuela.s@usach.cl>
2022-04-19 17:50:59 -0400

    Realizar cambios en el patron de diseño adapter

--- patrones_diseño/src/main/java/adapter/CuentaOrigen.java
+++ patrones_diseño/src/main/java/adapter/CuentaOrigen.java
@@ -2,6 +2,11 @@ package adapter;
 
 import java.math.BigDecimal;
 
+/**
+ * Una clase que representa una cuenta bancaria de origen a modo de cuenta corriente o cuenta viste que puede realizar
+ * transferencias a otras cuentas bancarias del tipo destino. Posee una cadena de texto que representa el numero de
+ * cuenta unico y un entero largo que representa el saldo de la cuenta.
+ */
 public class CuentaOrigen {
     private String numeroCuenta;
     private Long saldo;
@@ -11,12 +16,22 @@ public class CuentaOrigen {
         this.saldo = saldo;
     }
 
+    /**
+     * Metodo que permite realizar una transferencia desde una cuenta de origen a una determinada cuenta de destino,
+     * decontando el monto señalado a la cuenta de origen y recibiendolo al cuenta de destino.
+     * @param cuentaDestino una clase CuentaDestino que representa la cuenta que recibira el monto señalado
+     * @param monto un Long que representa el monto por el cual se esta realizando la tranferencia
+     */
     public void transferir(CuentaDestino cuentaDestino, Long monto){
         this.saldo=this.saldo-monto;
         cuentaDestino.recibirTransferencia(monto);
         System.out.println("Se ha realizado una transferencia por "+monto+" desde la cuenta numero "+this.numeroCuenta+"a la cuenta"+cuentaDestino.getNumeroCuenta());
     }
 
+    /**
+     * Metodo que permite obtener el nuemro de cuenta de la cuenta de origen
+     * @return Cadena de caracteres que contiene que representa el número de cuenta de la cuenta de origen
+     */
     public String getNumeroCuenta() {
         return numeroCuenta;
     }
--- patrones_diseño/src/main/java/adapter/mainAdapter.java
+++ patrones_diseño/src/main/java/adapter/mainAdapter.java
@@ -1,8 +1,15 @@
 package adapter;
 
+/**
+ * Clase cliente que permite ejemplifica el proceso de realizar un pago a una cuenta bancaria corriente/vista de destino
+ * haciendole un cargo a una cuenta de creditos utilizando el patron de diseño adapter. Cabe mencionar que la cuenta
+ * destino solo puede recibir transferencias si otra cuenta corriente/vista de origen.
+ */
 public class mainAdapter {
     public static void main(String[] args) {
+        //Definir la cuenta de de destino, la cuenta en donde se recibira el monto cargado
         CuentaDestino cuentaDestino=new CuentaDestino("1234567",5000L);
+
         CuentaCredito cuentaCredito=new CuentaCredito("1111-2222-3333-4444",1000L);
         new CuentaCreditoAdapter(cuentaCredito).creditoTransferencia(500L,cuentaDestino);
 
